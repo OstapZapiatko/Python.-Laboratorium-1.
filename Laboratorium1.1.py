81fe9814b3338360d0cf115f8e5cdb3bc8cc74a6
OstapZapiatko <117644024+OstapZapiatko@users.noreply.github.com>
2023-01-08 18:26:51 +0200

    Add files via upload

---
+++ Laboratorium1.1.py
@@ -0,0 +1,32 @@
+#print('''Zadanie. W linii Interpretera (Python Console) wykonaj poniższe działania. Za każdym razem sprawdź typ
+#wyników.''')
+
+a = 1 + 2;
+print(a)
+print(type(a))
+b = 1 + 4.5;
+print(b)
+print(type(b))
+c = 3/2
+print(c)
+print(type(c))
+d = 4/2
+print(d)
+print(type(d))
+e = 3//2
+print(e)
+print(type(e))
+f = -3//2
+print(f)
+print(type(f))
+g = 11%2
+print(g)
+print(type(g))
+i = 2**10
+print(i)
+print(type(i))
+j = 8**(1/3)
+print(j)
+print(type(j))
+
+
